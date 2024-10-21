7067d211ea7e197ab765ae6afd16e37cd1da7e11
z1glr <simon.ziegler.1998+github@pm.me>
2024-10-21 20:39:53 +0200

    Fixed "About"-text

--- client/src/BaseAbout.vue
+++ client/src/BaseAbout.vue
@@ -7,15 +7,12 @@ import BaseStatic from './BaseStatic.vue';
 	<BaseStatic>
 		<h1>About</h1>
 		<h2>Cookies</h2>
-		<p>Diese Seite nutzt ausschließlich technisch notwendige Cookies. Das
-		beinhaltet:</p>
+		<p>Diese Seite nutzt ausschließlich technisch notwendige Cookies. Das beinhaltet:</p>
 		<ul>
-		<li>Ausschließlich im Admin-Panel: Ein Session-Cookie zur
-		Authentifizierung mit dem Server. Beihnhaltet Benutzername, Benutzer-ID,
+		<li>Ausschließlich im Admin-Panel: Ein Session-Cookie zur Authentifizierung mit dem Server. Beihnhaltet Benutzername, Benutzer-ID,
 		Session-Token</li>
 		</ul>
-		<p>Als technisch notwendige Cookies benötigen diese keiner
-		Zustimmung.</p>
+		<p>Als technisch notwendiges Cookie benötigt dieses keiner Zustimmung.</p>
 	</BaseStatic>	
 </template>
 
